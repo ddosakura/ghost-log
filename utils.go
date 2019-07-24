0a98b2f0c9688fcc56733ea549b3946922c5fa28
Sakura Mo <moyinzi@outlook.com>
2019-07-25 02:30:05 +0800

    add new Logger Builder

--- utils.go
+++ utils.go
@@ -1,6 +1,22 @@
 package glog
 
+import (
+	"io"
+	"log"
+	"os"
+)
+
 // StopPanic util
 func StopPanic() {
 	_ = recover()
+}
+
+// MultiLogger Builder
+func MultiLogger(dir, fileName string, flag int) *log.Logger {
+	logFile, err := os.Open(dir + fileName + ".log")
+	if err != nil {
+		return nil
+	}
+	w := io.MultiWriter(os.Stderr, logFile)
+	return log.New(w, "["+fileName+"] ", flag)
 }
