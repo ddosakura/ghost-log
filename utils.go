53da4cbd00ac17cf4d09b9a3169ba2c91ae7536a
Sakura Mo <moyinzi@outlook.com>
2019-07-26 02:08:16 +0800

    bug fix

--- utils.go
+++ utils.go
@@ -13,8 +13,9 @@ func StopPanic() {
 
 // MultiLogger Builder
 func MultiLogger(dir, fileName string, flag int) *log.Logger {
-	logFile, err := os.Open(dir + fileName + ".log")
+	logFile, err := os.OpenFile(dir+fileName+".log", os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
+		log.Println(err)
 		return nil
 	}
 	w := io.MultiWriter(os.Stderr, logFile)
